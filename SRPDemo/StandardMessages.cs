854dae47c2eabfa0d4326d4d289a8c986bdd834f
Academy Guest <Academy Guest@LAPTOP-B107KD2O>
2022-08-29 09:50:10 +0100

    Add project files.

---
+++ SRPDemo/StandardMessages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRPDemo
+{
+    public class StandardMessages
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to my Application");
+        }
+        public static void EndApplication()
+        {
+            Console.Write("Press enter to close...");
+            Console.ReadLine();
+        }
+        public static void DisplayValidationError(string fieldName)
+        {
+            Console.WriteLine($"You did not give a valid { fieldName }!"); 
+        }
+    }
+}
